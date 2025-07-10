c0476d0a04887d71cf6ce830b189f9f50646b2d7
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-11 02:51:13 +0330

    - Add SYSTEMTIME Struct In Base.System.cs OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.System.cs
+++ OrcaUI.WinForms/Base/Base.System.cs
@@ -80,6 +80,18 @@ namespace OrcaUI.WinForms.Base
         public int dwHighDateTime;
     }
 
+    public struct SYSTEMTIME
+    {
+        public short wYear;
+        public short wMonth;
+        public short wDayOfWeek;
+        public short wDay;
+        public short wHour;
+        public short wMinute;
+        public short wSecond;
+        public short wMilliseconds;
+    }
+
 
 
 }
