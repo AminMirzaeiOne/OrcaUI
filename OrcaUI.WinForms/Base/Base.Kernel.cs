6a06005d13ecdda88da0d434e83cad28ed0410db
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-12 02:19:02 +0330

    - Add BY_HANDLE_FILE_INFORMATION Struct In Base.Kernel.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Kernel.cs
+++ OrcaUI.WinForms/Base/Base.Kernel.cs
@@ -197,4 +197,18 @@ namespace OrcaUI.WinForms.Base
         public int Reserved;
     }
 
+    public struct BY_HANDLE_FILE_INFORMATION
+    {
+        public int dwFileAttributes;
+        public FILETIME ftCreationTime;
+        public FILETIME ftLastAccessTime;
+        public FILETIME ftLastWriteTime;
+        public int dwVolumeSerialNumber;
+        public int nFileSizeHigh;
+        public int nFileSizeLow;
+        public int nNumberOfLinks;
+        public int nFileIndexHigh;
+        public int nFileIndexLow;
+    }
+
 }
