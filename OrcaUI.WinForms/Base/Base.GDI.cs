190fa1ecbf71201694bddfb721d34e443584a40a
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-11 17:24:32 +0330

    - Add ENHMETARECORD Struct In Base.GDI.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.GDI.cs
+++ OrcaUI.WinForms/Base/Base.GDI.cs
@@ -108,5 +108,12 @@ namespace OrcaUI.WinForms.Base
         public HANDLE hMF;
     }
 
+    public struct ENHMETARECORD
+    {
+        public int iType;
+        public int nSize;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)] public int dParm;
+    }
+
 
 }
