e632b17650e6be193769cfae242e18062b85459f
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-11 17:09:00 +0330

    - Add BITMAP Struct In Base.GDI.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.GDI.cs
+++ OrcaUI.WinForms/Base/Base.GDI.cs
@@ -15,4 +15,15 @@ namespace OrcaUI.WinForms.Base
         public double eDx;
         public double eDy;
     }
+
+    public struct BITMAP
+    {
+        public int bmType;
+        public int bmWidth;
+        public int bmHeight;
+        public int bmWidthBytes;
+        public short bmPlanes;
+        public short bmBitsPixel;
+        public int bmBits;
+    }
 }
