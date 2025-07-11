9e49cd59f16690eff7f0090389b9d7eff9bec42d
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-12 02:27:28 +0330

    - Add EVENTLOGRECORD Struct In Base.Kernel.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Kernel.cs
+++ OrcaUI.WinForms/Base/Base.Kernel.cs
@@ -222,4 +222,24 @@ namespace OrcaUI.WinForms.Base
         public int lType;
     }
 
+    public struct EVENTLOGRECORD
+    {
+        public int Length;
+        public int Reserved;
+        public int RecordNumber;
+        public int TimeGenerated;
+        public int TimeWritten;
+        public int EventID;
+        public short EventType;
+        public short NumStrings;
+        public short EventCategory;
+        public short ReservedFlags;
+        public int ClosingRecordNumber;
+        public int StringOffset;
+        public int UserSidLength;
+        public int UserSidOffset;
+        public int DataLength;
+        public int DataOffset;
+    }
+
 }
