39a64d3ce7f57f959e9c214e49e4e487de649e75
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-11 17:14:49 +0330

    - Add BITMAPFILEHEADER Struct In Base.GDI.cs OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.GDI.cs
+++ OrcaUI.WinForms/Base/Base.GDI.cs
@@ -78,5 +78,14 @@ namespace OrcaUI.WinForms.Base
         public RGBTRIPLE bmciColors;
     }
 
+    public struct BITMAPFILEHEADER
+    {
+        public short bfType;
+        public int bfSize;
+        public short bfReserved1;
+        public short bfReserved2;
+        public int bfOffBits;
+    }
+
 
 }
