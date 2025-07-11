fb0b278c81be95bbccc1e658fa6e005f4955222d
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-11 19:05:05 +0330

    - Add COMMPROP Struct In Base.Kernel.cs FIle OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Kernel.cs
+++ OrcaUI.WinForms/Base/Base.Kernel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,6 +29,28 @@ namespace OrcaUI.WinForms.Base
         public HANDLE hThread;
         public int dwProcessId;
         public int dwThreadId;
+    }
+
+    public struct COMMPROP
+    {
+        public short wPacketLength;
+        public short wPacketVersion;
+        public int dwServiceMask;
+        public int dwReserved1;
+        public int dwMaxTxQueue;
+        public int dwMaxRxQueue;
+        public int dwMaxBaud;
+        public int dwProvSubType;
+        public int dwProvCapabilities;
+        public int dwSettableParams;
+        public int dwSettableBaud;
+        public short wSettableData;
+        public short wSettableStopParity;
+        public int dwCurrentTxQueue;
+        public int dwCurrentRxQueue;
+        public int dwProvSpec1;
+        public int dwProvSpec2;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)] public short[] wcProvChar;
     }
 
 
