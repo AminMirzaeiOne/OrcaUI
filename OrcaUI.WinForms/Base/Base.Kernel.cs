f8423388762999ec8134b696b2beddfd83b8ce4c
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-12 02:51:34 +0330

    - Add ENUM_SERVICE_STATUS Struct In Base.Kernel.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Kernel.cs
+++ OrcaUI.WinForms/Base/Base.Kernel.cs
@@ -488,4 +488,11 @@ namespace OrcaUI.WinForms.Base
         public int dwWaitHint;
     }
 
+    public struct ENUM_SERVICE_STATUS
+    {
+        public string lpServiceName;
+        public string lpDisplayName;
+        public SERVICE_STATUS ServiceStatus;
+    }
+
 }
