a50ae4fe445b98def4e52da34449c3a724d734ab
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-12 02:10:38 +0330

    - Add SECURITY_DESCRIPTOR Struct In Base.Kernel.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Kernel.cs
+++ OrcaUI.WinForms/Base/Base.Kernel.cs
@@ -149,4 +149,15 @@ namespace OrcaUI.WinForms.Base
         public int AceSize;
     }
 
+    public struct SECURITY_DESCRIPTOR
+    {
+        public byte Revision;
+        public byte Sbz1;
+        public int Control;
+        public int Owner;
+        public int Group;
+        public ACL Sacl;
+        public ACL Dacl;
+    }
+
 }
