635980c72d0b2029a10f1158d940ddc987ce52d8
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-11 18:40:42 +0330

    - Add LOGFONT Struct In Base.GDI.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.GDI.cs
+++ OrcaUI.WinForms/Base/Base.GDI.cs
@@ -216,5 +216,24 @@ namespace OrcaUI.WinForms.Base
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)] public PALETTEENTRY palPalEntry;
     }
 
+    public struct LOGFONT
+    {
+        public int lfHeight;
+        public int lfWidth;
+        public int lfEscapement;
+        public int lfOrientation;
+        public int lfWeight;
+        public byte lfItalic;
+        public byte lfUnderline;
+        public byte lfStrikeOut;
+        public byte lfCharSet;
+        public byte lfOutPrecision;
+        public byte lfClipPrecision;
+        public byte lfQuality;
+        public byte lfPitchAndFamily;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string lfFaceName;
+    }
+
 
 }
