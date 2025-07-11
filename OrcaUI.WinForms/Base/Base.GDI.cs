94646397e00f2fdc5526d815d70ce5ceb72d628c
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-11 18:51:25 +0330

    - Add COLORADJUSTMENT Struct In Base.GDI.cs OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.GDI.cs
+++ OrcaUI.WinForms/Base/Base.GDI.cs
@@ -373,5 +373,21 @@ namespace OrcaUI.WinForms.Base
         public short nLanguageID;
     }
 
+    public struct COLORADJUSTMENT
+    {
+        public short caSize;
+        public short caFlags;
+        public short caIlluminantIndex;
+        public short caRedGamma;
+        public short caGreenGamma;
+        public short caBlueGamma;
+        public short caReferenceBlack;
+        public short caReferenceWhite;
+        public short caContrast;
+        public short caBrightness;
+        public short caColorfulness;
+        public short caRedGreenTint;
+    }
+
 
 }
