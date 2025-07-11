2cee568b8f190d7e33bd697d5046139e4c8960a1
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-12 01:36:16 +0330

    - Add DCB Struct In Base.Kernel.cs File OrcaUI.WInForms Project

--- OrcaUI.WinForms/Base/Base.Kernel.cs
+++ OrcaUI.WinForms/Base/Base.Kernel.cs
@@ -60,5 +60,24 @@ namespace OrcaUI.WinForms.Base
         public int cbOutQue;
     }
 
+    public struct DCB
+    {
+        public int DCBlength;
+        public int BaudRate;
+        public int fBitFields;
+        public short wReserved;
+        public short XonLim;
+        public short XoffLim;
+        public byte ByteSize;
+        public byte Parity;
+        public byte StopBits;
+        public byte XonChar;
+        public byte XoffChar;
+        public byte ErrorChar;
+        public byte EofChar;
+        public byte EvtChar;
+        public short wReserved1;
+    }
+
 
 }
