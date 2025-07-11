8c75b15650020e92920caab01fb80dd6e72b2346
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-11 18:40:05 +0330

    - Add LOGPALETTE Struct In Base.GDI.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.GDI.cs
+++ OrcaUI.WinForms/Base/Base.GDI.cs
@@ -209,5 +209,12 @@ namespace OrcaUI.WinForms.Base
         public byte peFlags;
     }
 
+    public struct LOGPALETTE
+    {
+        public short palVersion;
+        public short palNumEntries;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)] public PALETTEENTRY palPalEntry;
+    }
+
 
 }
