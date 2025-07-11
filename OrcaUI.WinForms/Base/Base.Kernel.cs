c2c9c4fde857353d0849898baa5c4977cda9c7e3
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-12 02:13:29 +0330

    - Add CRITICAL_SECTION Struct In Base.Kernel.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Kernel.cs
+++ OrcaUI.WinForms/Base/Base.Kernel.cs
@@ -187,4 +187,14 @@ namespace OrcaUI.WinForms.Base
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 128)] public byte[] szPathName;
     }
 
+    public struct CRITICAL_SECTION
+    {
+        public int pDebugInfo;
+        public int LockCount;
+        public int RecursionCount;
+        public int pOwningThread;
+        public int pLockSemaphore;
+        public int Reserved;
+    }
+
 }
