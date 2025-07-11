259b3d383937acafa2e7856850b9feb0106220ec
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-12 02:35:08 +0330

    - Add STARTUPINFO Structin In Base.Kernel.cs OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Kernel.cs
+++ OrcaUI.WinForms/Base/Base.Kernel.cs
@@ -352,4 +352,26 @@ namespace OrcaUI.WinForms.Base
         public int DaylightBias;
     }
 
+    public struct STARTUPINFO
+    {
+        public int cb;
+        public string lpReserved;
+        public string lpDesktop;
+        public string lpTitle;
+        public int dwX;
+        public int dwY;
+        public int dwXSize;
+        public int dwYSize;
+        public int dwXCountChars;
+        public int dwYCountChars;
+        public int dwFillAttribute;
+        public int dwFlags;
+        public short wShowWindow;
+        public short cbReserved2;
+        public int lpReserved2;
+        public HANDLE hStdInput;
+        public HANDLE hStdOutput;
+        public HANDLE hStdError;
+    }
+
 }
