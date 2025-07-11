3aa5917fcda26166b9a72f3c07bf5a78dbb3c37c
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-12 02:50:59 +0330

    - Add SERVICE_STATUS Struct In Base.Kernel.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Kernel.cs
+++ OrcaUI.WinForms/Base/Base.Kernel.cs
@@ -477,4 +477,15 @@ namespace OrcaUI.WinForms.Base
         public byte wcProviderData;
     }
 
+    public struct SERVICE_STATUS
+    {
+        public int dwServiceType;
+        public int dwCurrentState;
+        public int dwControlsAccepted;
+        public int dwWin32ExitCode;
+        public int dwServiceSpecificExitCode;
+        public int dwCheckPoint;
+        public int dwWaitHint;
+    }
+
 }
