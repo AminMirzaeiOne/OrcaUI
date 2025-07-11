8e3a6398389f4c84d81e0d37d393c4f37e1506fd
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-11 18:47:27 +0330

    - Add GLYPHMETRICS Struct In Base.GDI.cs OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.GDI.cs
+++ OrcaUI.WinForms/Base/Base.GDI.cs
@@ -351,5 +351,14 @@ namespace OrcaUI.WinForms.Base
         public FIXED eM22;
     }
 
+    public struct GLYPHMETRICS
+    {
+        public int gmBlackBoxX;
+        public int gmBlackBoxY;
+        public POINT gmptGlyphOrigin;
+        public short gmCellIncX;
+        public short gmCellIncY;
+    }
+
 
 }
