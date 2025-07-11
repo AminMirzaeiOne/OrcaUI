6e1c7e0522ff8690d4293443e31f1d9ce23cedb1
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-11 18:54:48 +0330

    - Add GCP_RESULTS Struct In Base.GDI.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.GDI.cs
+++ OrcaUI.WinForms/Base/Base.GDI.cs
@@ -416,5 +416,18 @@ namespace OrcaUI.WinForms.Base
         public FONTSIGNATURE fs;
     }
 
+    public struct GCP_RESULTS
+    {
+        public int lStructSize;
+        public string lpOutString;
+        public int lpOrder;
+        public int lpDX;
+        public int lpCaretPos;
+        public string lpClass;
+        public string lpGlyphs;
+        public int nGlyphs;
+        public int nMaxFit;
+    }
+
 
 }
