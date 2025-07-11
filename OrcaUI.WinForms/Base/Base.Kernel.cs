8372495ff15be67bc5e0ad4aac96b82577e66ebe
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-12 01:38:57 +0330

    - Add SYSTEM_INTO Struct In Base.Kernel.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Kernel.cs
+++ OrcaUI.WinForms/Base/Base.Kernel.cs
@@ -88,5 +88,17 @@ namespace OrcaUI.WinForms.Base
         public int WriteTotalTimeoutConstant;
     }
 
+    public struct SYSTEM_INFO
+    {
+        public int dwOemID;
+        public int dwPageSize;
+        public int lpMinimumApplicationAddress;
+        public int lpMaximumApplicationAddress;
+        public int dwActiveProcessorMask;
+        public int dwNumberOrfProcessors;
+        public int dwProcessorType;
+        public int dwAllocationGranularity;
+        public int dwReserved;
+    }
 
 }
