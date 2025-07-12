b9d8728ff7528c1a2c075b42b93c1606586f90a3
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-12 13:02:41 +0330

    - Add JOYINFOEX Struct In Base.Multimedia.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Multimedia.cs
+++ OrcaUI.WinForms/Base/Base.Multimedia.cs
@@ -84,4 +84,21 @@ namespace OrcaUI.WinForms.Base
         public int cbDetails;
         public int paDetails;
     }
+
+    public struct JOYINFOEX
+    {
+        public int dwSize;
+        public int dwFlags;
+        public int dwXpos;
+        public int dwYpos;
+        public int dwZpos;
+        public int dwRpos;
+        public int dwUpos;
+        public int dwVpos;
+        public int dwButtons;
+        public int dwButtonNumber;
+        public int dwPOV;
+        public int dwReserved1;
+        public int dwReserved2;
+    }
 }
