e40eecba82375d142f086200256cccce14217921
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-12 13:03:24 +0330

    - Add WAVEHDR Struct In Base.Multimedia.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Multimedia.cs
+++ OrcaUI.WinForms/Base/Base.Multimedia.cs
@@ -101,4 +101,16 @@ namespace OrcaUI.WinForms.Base
         public int dwReserved1;
         public int dwReserved2;
     }
+
+    public struct WAVEHDR
+    {
+        public string lpData;
+        public int dwBufferLength;
+        public int dwBytesRecorded;
+        public int dwUser;
+        public int dwFlags;
+        public int dwLoops;
+        public int lpNext;
+        public int Reserved;
+    }
 }
