7177629ce839c2d7556a8639870d5e4e98c71546
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-12 21:51:26 +0330

    - Add JOYCAPS Struct In Base.Multimedia.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Multimedia.cs
+++ OrcaUI.WinForms/Base/Base.Multimedia.cs
@@ -192,5 +192,21 @@ namespace OrcaUI.WinForms.Base
         public int wPeriodMax;
     }
 
+    public struct JOYCAPS
+    {
+        public short wMid;
+        public short wPid;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = WinMM.MAXPNAMELEN)] public string szPname;
+        public short wXmin;
+        public short wXmax;
+        public short wYmin;
+        public short wYmax;
+        public short wZmin;
+        public short wZmax;
+        public short wNumButtons;
+        public short wPeriodMin;
+        public short wPeriodMax;
+    }
+
 
 }
