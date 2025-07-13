bb688bca04c284db11b4d3e0acca6d0fcdf7486a
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-13 09:02:18 +0330

    - Add g_netSID_NAME_USE Struct In Base.NetApi.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.NetApi.cs
+++ OrcaUI.WinForms/Base/Base.NetApi.cs
@@ -56,5 +56,17 @@ namespace OrcaUI.WinForms.Base
         public int pSID;
     }
 
+    public enum g_netSID_NAME_USE
+    {
+        SidTypeUser = 1,
+        SidTypeGroup = 2,
+        SidTypeDomain = 3,
+        SidTypeAlias = 4,
+        SidTypeWellKnownGroup = 5,
+        SidTypeDeletedAccount = 6,
+        SidTypeInvalid = 7,
+        SidTypeUnknown = 8,
+    }
+
 
 }
