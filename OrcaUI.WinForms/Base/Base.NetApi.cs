0dcbef4e75f04b5d50b93968d915a4b6062c3636
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-13 08:59:19 +0330

    - Add NETRESOURCE Struct In Base.Multimedia.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.NetApi.cs
+++ OrcaUI.WinForms/Base/Base.NetApi.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace OrcaUI.WinForms.Base
 {
-    internal class Base
+    public struct NETRESOURCE
     {
+        public int dwScope;
+        public int dwType;
+        public int dwDisplayType;
+        public int dwUsage;
+        public string lpLocalName;
+        public string lpRemoteName;
+        public string lpComment;
+        public string lpProvider;
     }
+
+
 }
