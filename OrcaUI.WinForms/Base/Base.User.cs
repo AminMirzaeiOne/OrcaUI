fbe8e97e909319b4a12c9494f6fdb8d66c9f0335
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-13 09:41:07 +0330

    - Add CONVCONTEXT Struct In Base.User.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.User.cs
+++ OrcaUI.WinForms/Base/Base.User.cs
@@ -117,4 +117,16 @@ namespace OrcaUI.WinForms.Base
     }
 
 
+    public struct CONVCONTEXT
+    {
+        public int cb;
+        public int wFlags;
+        public int wCountryID;
+        public int iCodePage;
+        public int dwLangID;
+        public int dwSecurity;
+        public SECURITY_QUALITY_OF_SERVICE qos;
+    }
+
+
 }
