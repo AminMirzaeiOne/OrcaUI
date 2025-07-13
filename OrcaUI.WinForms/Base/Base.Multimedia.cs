cbd8e4621e3b9c9b74b9d419f3f222813cbea3ca
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-13 08:51:00 +0330

    - Add MMIOINFO Struct In Base.Multimedia.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.Multimedia.cs
+++ OrcaUI.WinForms/Base/Base.Multimedia.cs
@@ -216,5 +216,25 @@ namespace OrcaUI.WinForms.Base
         public int wButtons;
     }
 
+    public struct MMIOINFO
+    {
+        public int dwFlags;
+        public int fccIOProc;
+        public int pIOProc;
+        public int wErrorRet;
+        public HANDLE htask;
+        public int cchBuffer;
+        public string pchBuffer;
+        public string pchNext;
+        public string pchEndRead;
+        public string pchEndWrite;
+        public int lBufOffset;
+        public int lDiskOffset;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)] public int adwInfo;
+        public int dwReserved1;
+        public int dwReserved2;
+        public HANDLE hmmio;
+    }
+
 
 }
