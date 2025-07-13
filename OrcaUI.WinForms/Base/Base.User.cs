d8fd58c3d8bffbebe8bef27509dd72a67f6b399c
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-13 09:38:53 +0330

    - Add DLGTEMPLATE Struct In Base.User.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.User.cs
+++ OrcaUI.WinForms/Base/Base.User.cs
@@ -88,5 +88,16 @@ namespace OrcaUI.WinForms.Base
         public POINT pt;
     }
 
+    public struct DLGTEMPLATE
+    {
+        public int style;
+        public int dwExtendedStyle;
+        public short cdit;
+        public short x;
+        public short y;
+        public short cx;
+        public short cy;
+    }
+
 
 }
