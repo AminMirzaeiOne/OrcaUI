03b726b54174f36b2ec2d1e637a7eb592e440510
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-13 09:29:49 +0330

    - Add PAINTSTRUCT Struct In Base.User.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Base/Base.User.cs
+++ OrcaUI.WinForms/Base/Base.User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -37,6 +38,18 @@ namespace OrcaUI.WinForms.Base
         public byte fVirt;
         public short key;
         public short cmd;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PAINTSTRUCT
+    {
+        public IntPtr hdc;
+        public bool fErase;
+        public RECT rcPaint;
+        public bool fRestore;
+        public bool fIncUpdate;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+        public byte[] rgbReserved;
     }
 
 
