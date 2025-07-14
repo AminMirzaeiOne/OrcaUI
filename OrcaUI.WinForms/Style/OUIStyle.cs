5a92829a3bf3ae818d251460592603fe4ac20dbe
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-15 01:15:51 +0330

    - Add OUIColor In OUIStyle.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Style/OUIStyle.cs
+++ OrcaUI.WinForms/Style/OUIStyle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -103,6 +104,148 @@ namespace OrcaUI.WinForms.Style
         [Description("Colorful")]
         Colorful = 999
     }
+
+    /// <summary>
+    /// Theme colors
+    /// </summary>
+    public static class OUIColor
+    {
+        /// <summary>
+        /// Blue
+        /// </summary>
+        public static readonly Color Blue = Color.FromArgb(80, 160, 255);
+
+        /// <summary>
+        /// Green
+        /// </summary>
+        public static readonly Color Green = Color.FromArgb(110, 190, 40);
+
+        /// <summary>
+        /// Red
+        /// </summary>
+        public static readonly Color Red = Color.FromArgb(230, 80, 80);
+
+        /// <summary>
+        /// Gray
+        /// </summary>
+        public static readonly Color Gray = Color.FromArgb(140, 140, 140);
+
+        /// <summary>
+        /// Orange
+        /// </summary>
+        public static readonly Color Orange = Color.FromArgb(220, 155, 40);
+
+        /// <summary>
+        /// Layui Green
+        /// </summary>
+        public static readonly Color LayuiGreen = Color.FromArgb(0, 150, 136);
+
+        /// <summary>
+        /// Layui Red
+        /// </summary>
+        public static readonly Color LayuiRed = Color.FromArgb(255, 87, 34);
+
+        /// <summary>
+        /// Layui Orange
+        /// </summary>
+        public static readonly Color LayuiOrange = Color.FromArgb(255, 184, 0);
+
+        /// <summary>
+        /// Layui Cyan
+        /// </summary>
+        public static readonly Color LayuiCyan = Color.FromArgb(46, 57, 79);
+
+        /// <summary>
+        /// Layui Blue
+        /// </summary>
+        public static readonly Color LayuiBlue = Color.FromArgb(69, 149, 255);
+
+        /// <summary>
+        /// Layui Black
+        /// </summary>
+        public static readonly Color LayuiBlack = Color.FromArgb(52, 55, 66);
+
+        /// <summary>
+        /// Dark Blue
+        /// </summary>
+        public static readonly Color DarkBlue = Color.FromArgb(14, 30, 63);
+
+        /// <summary>
+        /// White
+        /// </summary>
+        public static readonly Color White = Color.White;
+
+        /// <summary>
+        /// Black
+        /// </summary>
+        public static readonly Color Black = Color.Black;
+
+        /// <summary>
+        /// Purple
+        /// </summary>
+        public static readonly Color Purple = Color.FromArgb(102, 58, 183);
+
+        /// <summary>
+        /// Light Purple
+        /// </summary>
+        public static readonly Color LightPurple = Color.FromArgb(250, 238, 255);
+
+        /// <summary>
+        /// Transparent
+        /// </summary>
+        public static readonly Color Transparent = Color.Transparent;
+
+        /// <summary>
+        /// Light Blue
+        /// </summary>
+        public static readonly Color LightBlue = Color.FromArgb(235, 243, 255);
+
+        /// <summary>
+        /// Light Green
+        /// </summary>
+        public static readonly Color LightGreen = Color.FromArgb(239, 248, 232);
+
+        /// <summary>
+        /// Light Red
+        /// </summary>
+        public static readonly Color LightRed = Color.FromArgb(251, 238, 238);
+
+        /// <summary>
+        /// Light Gray
+        /// </summary>
+        public static readonly Color LightGray = Color.FromArgb(242, 242, 244);
+
+        /// <summary>
+        /// Light Orange
+        /// </summary>
+        public static readonly Color LightOrange = Color.FromArgb(251, 245, 233);
+
+        /// <summary>
+        /// Medium Blue
+        /// </summary>
+        public static readonly Color RegularBlue = Color.FromArgb(216, 233, 255);
+
+        /// <summary>
+        /// Medium Green
+        /// </summary>
+        public static readonly Color RegularGreen = Color.FromArgb(224, 242, 210);
+
+        /// <summary>
+        /// Medium Red
+        /// </summary>
+        public static readonly Color RegularRed = Color.FromArgb(248, 222, 222);
+
+        /// <summary>
+        /// Medium Gray
+        /// </summary>
+        public static readonly Color RegularGray = Color.FromArgb(230, 230, 232);
+
+        /// <summary>
+        /// Medium Orange
+        /// </summary>
+        public static readonly Color RegularOrange = Color.FromArgb(247, 234, 210);
+    }
+
 
 
 
