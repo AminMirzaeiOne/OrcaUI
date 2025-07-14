c6055ef82a2754ee6e3839eea6fc3dff9fd24a7f
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-14 13:26:42 +0330

    - Add and Complete IStyleInterface Interface In OUIStyle.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Style/OUIStyle.cs
+++ OrcaUI.WinForms/Style/OUIStyle.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace OrcaUI.WinForms.Style
 {
-    internal class OUIStyle
+    public interface IStyleInterface
     {
+        UIStyle Style { get; set; }
+
+        string Version { get; }
+
+        string TagString { get; set; }
+
+        void SetStyleColor(UIBaseStyle uiColor);
+
+        //void SetStyle(UIStyle style);
+        void SetInheritedStyle(UIStyle style);
+
+        void SetDPIScale();
     }
 }
