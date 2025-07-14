ddee26a3ff1737713df88c30b39c4c615bcc740d
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-14 13:21:42 +0330

    - Add Luminosity Enum In OUIRandomColor.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Style/OUIRandomColor.cs
+++ OrcaUI.WinForms/Style/OUIRandomColor.cs
@@ -412,4 +412,27 @@ namespace OrcaUI.WinForms.Style
         Pink
     }
 
+    /// <summary>
+    /// Predefined luminosity ranges.
+    /// </summary>
+    public enum Luminosity
+    {
+        /// <summary>
+        /// Select randomly from among the other luminosities.
+        /// </summary>
+        Random,
+        /// <summary>
+        /// Generate dark colors.
+        /// </summary>
+        Dark,
+        /// <summary>
+        /// Generate light, pastel colors.
+        /// </summary>
+        Light,
+        /// <summary>
+        /// Generate vibrant colors.
+        /// </summary>
+        Bright,
+    }
+
 }
