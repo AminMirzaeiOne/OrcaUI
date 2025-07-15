5823c074763b5fd3e91241c47cde60406bdf7ea1
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-15 11:05:19 +0330

    - Add CreateTextureBrush Function In OUITexture Class OrcaUI.WinForms Project

--- OrcaUI.WinForms/Theme/OUITexture.cs
+++ OrcaUI.WinForms/Theme/OUITexture.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,11 @@ namespace OrcaUI.WinForms.Theme
 {
     public static class OUITexture
     {
-
+        public static TextureBrush CreateTextureBrush(Image img)
+        {
+            TextureBrush tb = new TextureBrush(img);
+            tb.WrapMode = System.Drawing.Drawing2D.WrapMode.Tile;
+            return tb;
+        }
     }
 }
