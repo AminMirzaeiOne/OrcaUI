ed10ed784dbd3567e295858597ea31f80ac3f4ae
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-15 11:10:08 +0330

    - Add IZoomScale Interface In OUIZoomScale.cs File OrcaUI.WinForms Project

--- OrcaUI.WinForms/Theme/OUIZoomScale.cs
+++ OrcaUI.WinForms/Theme/OUIZoomScale.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OrcaUI.WinForms.Theme
 {
-    
+    public interface IZoomScale
+    {
+        /// <summary>
+        /// Set the control zoom scale
+        /// </summary>
+        /// <param name="scale">Zoom scale</param>
+        void SetZoomScale(float scale);
+
+        /// <summary>
+        /// Position of the control in its container before zoom
+        /// </summary>
+        Rectangle ZoomScaleRect { get; set; }
+
+        /// <summary>
+        /// Disable the control from following form zoom
+        /// </summary>
+        bool ZoomScaleDisabled { get; set; }
+    }
 }
