52be1803ffceeaca03b92b4663e3f4e06ee93520
aminmirzaeione <aminmirzaei.og@gmail.com>
2025-07-16 10:12:02 +0330

    - Add TransfromNeededEventArg Class In OCAnimator Project

--- OrcaUI.Animation/OCAnimator.cs
+++ OrcaUI.Animation/OCAnimator.cs
@@ -724,5 +724,22 @@ namespace OrcaUI.Animation
         public AnimateMode Mode { get; internal set; }
     }
 
+    public class TransfromNeededEventArg : EventArgs
+    {
+        public TransfromNeededEventArg()
+        {
+            Matrix = new Matrix(1, 0, 0, 1, 0, 0);
+        }
+
+        public Matrix Matrix { get; set; }
+        public float CurrentTime { get; internal set; }
+        public Rectangle ClientRectangle { get; internal set; }
+        public Rectangle ClipRectangle { get; internal set; }
+        public Animation Animation { get; set; }
+        public Control Control { get; internal set; }
+        public AnimateMode Mode { get; internal set; }
+        public bool UseDefaultMatrix { get; set; }
+    }
+
 
 }
